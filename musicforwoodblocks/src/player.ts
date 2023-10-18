8649fe150fe2e4c543dfb503454086c77247ec22
Enno <h.j.meijers@gmail.com>
2023-10-18 15:41:08 +0200

    Setup for adding score data

--- musicforwoodblocks/src/player.ts
+++ musicforwoodblocks/src/player.ts
@@ -1,12 +1,23 @@
 import * as Tone from "tone";
 import { woodblock } from "./graph";
 
-export function start() {
-  Tone.Transport.bpm.value = 240;
+import scoreData from './parts.json';
 
-  new Tone.Loop((time) => {
-    woodblock.triggerAttackRelease("C5", "0:1", time)
-  }, "3m").start();
+export function makePart( partData:any ) {
+  const part = new Tone.Part(((time, note) => {
+    woodblock.triggerAttackRelease(note, "8n", time);
+  }), partData.pattern );
+  part.loop = partData.loop;
+  part.start(partData.start);
+}
+
+export function start() {
+  Tone.Transport.bpm.value = 108;
+  Tone.Transport.timeSignature = 3 
+
+  for(const partNumber in scoreData){
+    makePart(scoreData[partNumber]);
+  }
 
   Tone.Transport.start();
   Tone.start();
